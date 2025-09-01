a6c1720389fd4bdd3894ec15c8a51b2a82509fc7
Sammy Njue <sammynjue10@gmail.com>
2025-09-01 07:05:25 +0300

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,9 +1,9 @@
 import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "";
+const SUPABASE_URL = "https://dabljjonrpbnidwnkwgz.supabase.co";
 const SUPABASE_ANON_KEY =
-  "";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYmxqam9ucnBibmlkd25rd2d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExNjIzMTIsImV4cCI6MjA0NjczODMxMn0.heJ-OmceVakcQElnBp7tXYsxyHnMR5hhg4xR6R0A03o";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
